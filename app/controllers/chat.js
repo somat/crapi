2b4b0f7f9ccd760d334051bb3453aa1fe0381c25
Akhmat Safrudin <somat@artikulpi.com>
2017-10-17 18:26:50 +0700

    New: add chat endpoint.

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -62,4 +62,43 @@ ChatController.getOne = function(req, res) {
   }
 }
 
+/**
+ * Add new corpus
+ * @param  {Object}   req  Request object
+ * @param  {Object}   res  Response object
+ * @return {Object}        Response object
+ */
+ChatController.add = function(req, res) {
+  req.assert('enc', 'Encoder cannot be empty').notEmpty()
+  req.assert('dec', 'Decoder cannot be empty').notEmpty()
+  error = req.validationErrors()
+
+  if(error) {
+    res.status(200).json({
+      success: false,
+      message: 'Validation error.',
+      data: {}
+    })
+  } else {
+    Chat.create({
+      enc: req.body.enc,
+      dec: req.body.dec
+    })
+    .then(function(data) {
+      res.status(200).json({
+        success: true,
+        message: 'Create chat success',
+        data: data
+      })
+    })
+    .catch(function(err) {
+      res.status(500).json({
+        success: false,
+        message: err,
+        data: {}
+      })
+    })
+  }
+}
+
 module.exports = ChatController
--- app/router.js
+++ app/router.js
@@ -9,5 +9,6 @@ router.get('/', function(req, res) {res.json({message: 'Hello.'})})
 // chat
 router.post(url.chat_list, chat.getAll)
 router.post(url.chat_view, chat.getOne)
+router.post(url.chat_add, chat.add)
 
 module.exports = router
